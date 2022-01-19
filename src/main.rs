28f3b79ee5f55429cc544e009bcf8f46eef9b765
hooneun <kimhoon0316@gmail.com>
2022-01-19 21:57:55 +0900

    responses

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router, Json};
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(root))
-        .route("/foo", get(get_foo).post(post_foo))
-        .route("/foo/bar", get(foo_bar));
+        .route("/plain_text", get(plain_text))
+        .route("/json", get(json));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -13,18 +13,10 @@ async fn main() {
         .unwrap();
 }
 
-async fn root() -> String {
-    "get Root!".to_string()
+async fn plain_text() -> &'static str {
+    "foo"
 }
 
-async fn get_foo() -> String {
-    "get Foo".to_string()
-}
-
-async fn post_foo() -> String {
-    "post Foo".to_string()
-}
-
-async fn foo_bar() -> String {
-    "get FooBar".to_string()
+async fn json() -> Json<Value>{
+    Json(json!({ "data": 42 }))
 }
