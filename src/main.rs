1e750c8012653d94a68fbf2377b04377c6406767
hooneun <kimhoon0316@gmail.com>
2022-02-04 13:46:44 +0900

    Read a os env port

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use axum::{routing::get, Router, Json};
+use std::net::SocketAddr;
+
+use axum::{routing::get, Json, Router};
 use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
+    let port = std::env::var("PORT").unwrap_or("3000".to_string());
+
     let app = Router::new()
         .route("/plain_text", get(plain_text))
         .route("/json", get(json));
 
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    let addr = SocketAddr::from(([127, 0, 0, 1], port.parse().unwrap()));
+
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
@@ -17,6 +23,6 @@ async fn plain_text() -> &'static str {
     "foo"
 }
 
-async fn json() -> Json<Value>{
+async fn json() -> Json<Value> {
     Json(json!({ "data": 42 }))
 }
